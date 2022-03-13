3c172fc639e3d005e762f9362968443c379211b5
MarieCHessler <marie@hessler.se>
2022-03-13 15:20:35 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -6,7 +6,42 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr); // Takes two parameters, accumulator and currentValue
+console.log(sum)
 
+let sum2 = nums.reduce((acc, curr) => { // Expanded version of above code 
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}); // No specified initial value to use as accumulator means it defaults to the first element in the array and the process starts with the second element and the callback function will execute four times
+console.log(sum2)
+
+let sum3 = nums.reduce((acc, curr) => { // Expanded version of above code 
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 0); // Specified initial value to use as accumulator means the function executes five times
+console.log(sum3)
+
+let sum4 = nums.reduce((acc, curr) => { // Expanded version of above code 
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 10); // Specified initial value to use as accumulator means the function executes five times
+console.log(sum4)
+
+let sum5 = nums.reduce((acc, curr) => acc + curr, 0); // Takes two parameters, accumulator and currentValue
+console.log(sum5)
 
 const teamMembers = [
   {
